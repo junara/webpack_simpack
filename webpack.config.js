43336806f5cc24ea65238ea3bcfd16b2417cd1c3
junara <jung5araki@gmail.com>
2021-07-23 14:21:07 +0900

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -22,16 +22,26 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "public/packs"),
-    publicPath: "/packs/",
+    publicPath: isProd ? "/packs/" : "//localhost:8081/packs/",
     filename: isProd ? "[name]-[hash].js" : "[name].js",
   },
   resolve: {
     extensions: [".js"],
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    publicPath: "/packs/",
+    host: "localhost",
+    port: 8081,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
   plugins: [
     new WebpackAssetsManifest({
       publicPath: true,
       output: "manifest.json",
+      writeToDisk: true,
     }),
   ],
 };
